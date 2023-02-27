3105f1ffdd2fc9b7b77cf3cecb1bbecb60e05302
Bipin Dahal <bpn11dahal@gmail.com>
2023-02-27 11:03:23 +0545

    design complete - buttons added - search bar added - api function completed

--- src/api/API.js
+++ src/api/API.js
@@ -1,17 +1,19 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { apiKey } from '../constants/config';
 
 const API = ({ query, category }) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  const baseURL =
+    'https://api.flickr.com/services/rest/?method=flickr.photos.search';
   useEffect(() => {
     let cancelRequest = false;
 
     setIsLoading(true);
 
     const fetchPhotos = async () => {
-      const apiURL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=636e1481b4f3c446d26b8eb6ebfe7127&tags=${query},${category}&per_page=24&format=json&nojsoncallback=1`;
+      const apiURL = `${baseURL}&api_key=${apiKey}&tags=${query},${category}&per_page=24&format=json&nojsoncallback=1`;
       try {
         const response = await axios.get(apiURL);
         const data = response.data;
